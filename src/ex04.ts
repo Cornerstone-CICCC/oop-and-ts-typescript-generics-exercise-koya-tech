248b8c510f8c6de5c71b8687093eaf76e00f2248
koya <koya9431ptr@gmail.com>
2025-02-11 20:24:50 +0000

    done

--- src/ex04.ts
+++ src/ex04.ts
@@ -2,20 +2,20 @@
 // Add methods to `getValue` and `setValue`.
 // Initialize it with "Initial Value", update it to "Updated Value", and log the new value.
 
-class Container {
+class Container<T> {
   private value;
 
-  constructor(value) {
+  constructor(value:T) {
     this.value = value;
   }
 
   getValue = () => this.value;
 
-  setValue = (value) => {
+  setValue = (value:T) => {
     this.value = value;
   };
 }
 
-const stringContainer = new Container("Initial Value");
+const stringContainer = new Container<string>("Initial Value");
 stringContainer.setValue("Updated Value");
 console.log(stringContainer.getValue()); // Expected output: "Updated Value"
